6278cb114a4e81e09247100f2f2ab00740710000
unknown <dan.harabagiu.dimitrescu@gmail.com>
2024-10-27 16:03:21 +0200

    final build

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,33 @@ function App() {
 
   return (
     <>
-     
+     <div className='bg-veryDarkBlue h-screen w-full grid place-content-center'>
+      <div className='bg-darkSaturatedBlue w-80 md:w-auto rounded-md flex flex-col-reverse md:flex-row font-Lexend'>
+        <div className='p-5 text-center md:text-left md:p-10 md:pr-28 md:w-5/6'>
+          <div>
+          <h1 className='text-white text-3xl md:text-3xl font-bold font-Inter'>Get <span className='text-softViolet'>insights</span> that help your business grow.</h1>
+          <p className='text-paragraph mt-5 md:mt-7 text-md'>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</p>
+          </div>
+            <div className='flex md:flex-row flex-col justify-between mt-5 md:mt-20'>
+              <div className='my-5 md:my-0'>
+                <h2 className='text-white font-Inter font-bold text-2xl'>10k+</h2>
+                <p className='text-heading text-xs'>COMPANIES</p>
+              </div>
+              <div className='my-5 md:my-0'>
+                <h2 className='text-white font-Inter font-bold text-2xl'>314</h2>
+                <p className='text-heading text-xs'>TEMPLATES</p>
+              </div>
+              <div className='my-5 md:my-0'>
+                <h2 className='text-white font-Inter font-bold text-2xl'>12M+</h2>
+                <p className='text-heading text-xs'>QUERIES</p>
+              </div>
+            </div>
+        </div>
+        <div className='bg-desktop h-60 bg-cover md:w-full md:h-full bg-no-repeat rounded-r-md'>
+          <div className='w-full h-full bg-softViolet mix-blend-multiply'></div>
+        </div>
+      </div>
+     </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,17 @@ export default {
       colors: {
         'paragraph': 'hsla(0, 0%, 100%, 0.75)',
         'heading': 'hsla(0, 0%, 100%, 0.6)',
-        'varyDarkBlue': 'hsl(233, 47%, 7%)',
+        'veryDarkBlue': 'hsl(233, 47%, 7%)',
         'darkSaturatedBlue': 'hsl(244, 38%, 16%)',
         'softViolet': 'hsl(277, 64%, 61%)'
       },
       fontFamily: {
-        'Inter': ['Inter', 'sans-serif']
+        'Inter': ['Inter', 'sans-serif'],
+        'Lexend': ['Lexend Deca', 'sans-serif']
+      },
+      backgroundImage: {
+        'desktop': "url('/image-header-desktop.jpg')",
+        'mobile': "url('/image-header-mobile.jpg')"
       }
     },
   },
